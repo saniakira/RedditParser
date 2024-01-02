e4cba3e8e6d3d990a040fb6695da9e4918f2001e
saniakira <s.balmacova@gmail.com>
2024-01-02 15:27:08 +0300

    parse done

--- parser/parser.py
+++ parser/parser.py
@@ -1,23 +1,37 @@
+import os
+
+import deep_translator
 import lxml
 from bs4 import BeautifulSoup
 import requests
 from icecream import ic
 
 
-def parse_img(
-        url="https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fhmm-hmmhmmm-v0-t3djq4rqin9c1.jpeg%3Fs%3D3bb62593a4cb3f4bfea7c2bc58287c976eb36bdc"):
+# def parse_img(
+#         url="https://i.redd.it/me-everyone-that-post-those-would-u-let-misato-in-a-damn-v0-pml7c8chu5lb1.jpg?s=b8873b74a381217a55f29cff749f691cfbd617cc"):
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.content, 'lxml')
+#     ic(soup)
+# response = soup.find('meta', property='og:image')
+# print(str(response).split('"'))
+# response = str(response).split('"')[1]
+# img = requests.get(response)
+# with open('response.jpg', 'wb') as f:
+#     f.write(img.content)
+
+
+def parse(url='https://www.reddit.com/r/evangelionmemes/'):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
-    response = soup.find('meta', property='og:image')
-    response = str(response).split('"')[1]
-    img = requests.get(response)
-    with open('response.jpg', 'wb') as f:
-        f.write(img.content)
+    responses = soup.find_all('article')
+    i = 1
+    for response in responses:
+        text = str(response.find('a', slot='title')).split('slot="title">')[1].split('<')[0]
+        img = requests.get(str(response).split('content-href=')[1].split('"')[1])
+        with open(f"{os.getcwd()}\\{deep_translator.GoogleTranslator('auto', 'ru').translate(text)}.jpg",
+                  'wb') as f:
+            f.write(img.content)
+        i += 1
 
 
-def parse(url='https://www.reddit.com/r/evangelion/'):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'lxml')
-    response = soup.find_all('article')
-    ic(len(response))
-
+parse()
--- text_addition/add_text.py
+++ text_addition/add_text.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 import deep_translator
 
 
-def add_text(text: str, path: str) -> None:
+def add_text(text: str, path="") -> None:
     im = Image.new('RGB', (1280, 720), color=('#FAACAC'))
     draw_text = ImageDraw.Draw(im)
     draw_text.text(
